d70991b5f2eca8f7edc514cbc4f12c6535ccdcdc
Steve Owens <steven_owens@epam.com>
2020-11-18 15:41:25 +0000

    Updared Checkbox source link

--- src/calculator.app/src/tests/components/CheckboxWithLabels.test.js
+++ src/calculator.app/src/tests/components/CheckboxWithLabels.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {cleanup, fireEvent, render} from '@testing-library/react';
-import CheckboxWithLabel from '../../Components/CheckboxWithLabel';
+import CheckboxWithLabel from '../../components/CheckboxWithLabel';
 
 // Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
 // unmount and cleanup DOM after the test is finished.
